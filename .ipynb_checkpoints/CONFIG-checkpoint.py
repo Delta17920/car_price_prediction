e799409aeb45336490db15c171ef16a5f69d6af2
Pratik <pratik161106@gmail.com>
2024-10-29 22:39:01 +0530

    AI

---
+++ .ipynb_checkpoints/CONFIG-checkpoint.py
@@ -0,0 +1,7 @@
+HOST = "0.0.0.0"
+PORT = 8000
+DEBUG = False
+
+MODEL_PATH =r"/home/ubuntu/car_selling_price_prediction/app/Car_Pred_Model.pkl"
+COLUMNS_PATH = r"/home/ubuntu/car_selling_price_prediction/app/Column.json"
+ENCODE_PATH = r"/home/ubuntu/car_selling_price_prediction/app/encoded_data.json"
